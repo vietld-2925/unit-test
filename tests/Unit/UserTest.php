fcaa13054120a81151debdd75fd50944a228b51e
le.duc.viet <le.duc.viet@sun-asterisk.com>
2022-03-29 22:31:14 +0700

    bỏ bớt test case th

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -57,38 +57,8 @@ class UserTest extends TestCase
         $this->assertEquals(0,$testSun->ex2('2022-03-27 09:00:00',true,true));
     }
 
+
     public function test_ex3()
-    {
-        $testSun = new TestSun();
-        $this->assertEquals(110,$testSun->ex2('2022-03-29 00:20:20',false,false));
-        $this->assertEquals(0,$testSun->ex2('2022-03-29 09:00:00',false,false));
-        $this->assertEquals(110,$testSun->ex2('2022-03-29 18:00:00',false,false));
-
-        /*Chu Nhat, Ngay le*/
-        $this->assertEquals(110,$testSun->ex2('2022-03-29 09:00:00',false,true));
-        $this->assertEquals(110,$testSun->ex2('2022-03-27 09:00:00',false,false));
-        $this->assertEquals(110,$testSun->ex2('2022-03-27 09:00:00',false,true));
-
-        /*Khach vip*/
-        $this->assertEquals(0,$testSun->ex2('2022-03-29 00:20:20',true,false));
-        $this->assertEquals(0,$testSun->ex2('2022-03-29 18:00:00',true,false));
-        $this->assertEquals(0,$testSun->ex2('2022-03-29 09:00:00',true,false));
-
-        $this->assertEquals(0,$testSun->ex2('2022-03-29 00:20:20',true,true));
-        $this->assertEquals(0,$testSun->ex2('2022-03-29 18:00:00',true,true));
-        $this->assertEquals(0,$testSun->ex2('2022-03-29 09:00:00',true,true));
-
-
-        $this->assertEquals(0,$testSun->ex2('2022-03-27 00:20:20',true,false));
-        $this->assertEquals(0,$testSun->ex2('2022-03-27 18:00:00',true,false));
-        $this->assertEquals(0,$testSun->ex2('2022-03-27 09:00:00',true,false));
-
-        $this->assertEquals(0,$testSun->ex2('2022-03-27 00:20:20',true,true));
-        $this->assertEquals(0,$testSun->ex2('2022-03-27 18:00:00',true,true));
-        $this->assertEquals(0,$testSun->ex2('2022-03-27 09:00:00',true,true));
-    }
-
-    public function test_ex4()
     {
         $testSun = new TestSun();
         $this->assertEquals(7,$testSun->ex3(['So Mi','Quan','Quan','Quan','Quan','Quan','Quan','Quan','Quan']));
