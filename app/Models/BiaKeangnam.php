dbcf6339d1608a31545d7e5ea0243ce20da39293
le.duc.viet <le.duc.viet@sun-asterisk.com>
2022-03-30 09:34:25 +0700

    Fix_B1_Coding_Convention

---
+++ app/Models/BiaKeangnam.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BiaKeangnam extends Model
+{
+    use HasFactory;
+
+    public function calculatePrice($time='18:10', $hasVoucher=false, $isFirst=false){
+        $data = [
+            'price' => 490,
+            'voucher' => 100,
+            'promotion' => [
+                'start' => '16:00',
+                'end' => '17:59',
+                'price' => 290
+            ],
+        ];
+        $result = $data['price'];
+        if($time <= $data['promotion']['end'] && $time >= $data['promotion']['start']){
+            $result = $data['promotion']['price'];
+        }
+
+        if($hasVoucher && $isFirst){
+            $result = $data['voucher'];
+        }
+        return $result;
+    }
+}
---
+++ app/Models/QuanAoHoanKiem.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class QuanAoHoanKiem extends Model
+{
+    use HasFactory;
+}
